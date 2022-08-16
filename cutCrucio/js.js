70663d64b25ddec42b86431d2addf2a776dc14b4
NguyenMinh1009 <minhnnm1009@gmail.com>
2022-08-16 13:46:04 +0700

    Upfile cutCrucio 16/8

---
+++ cutCrucio/js.js
@@ -0,0 +1,79 @@
+const toggleButton = document.getElementById('toggle-button');
+const naviList = document.getElementById('navi-list');
+
+toggleButton.addEventListener('click', () => {
+    naviList.classList.toggle('active');
+
+})
+
+/*----------------------------------------------------------------------------------------*/
+
+
+// for (let i = 0; i <= 2; i++) {
+//     document.getElementsByClassName('mask')[i].style.display = "none";
+// }
+
+function myonmou1() {
+    document.querySelector('#team > div > div.col-md-8.row > div:nth-child(1) > img').style.transform = 'scale(1.2)';
+    document.querySelector('#team > div > div.col-md-8.row > div:nth-child(1) > div').style.transform = 'scale(1.2)';
+    // document.getElementsByClassName('mask')[0].style.display = "block";
+}
+
+function myonmou2() {
+    document.querySelector('#team > div > div.col-md-8.row > div:nth-child(2) > img').style.transform = 'scale(1.2)';
+    document.querySelector('#team > div > div.col-md-8.row > div:nth-child(2) > div').style.transform = 'scale(1.2)';
+    // document.getElementsByClassName('mask')[1].style.display = "block";
+}
+
+function myonmou3() {
+    document.querySelector('#team > div > div.col-md-8.row > div:nth-child(3) > img').style.transform = 'scale(1.2)';
+    document.querySelector('#team > div > div.col-md-8.row > div:nth-child(3) > div').style.transform = 'scale(1.2)';
+    // document.getElementsByClassName('mask')[2].style.display = "block";
+}
+
+
+function myout() {
+    document.querySelector('#team > div > div.col-md-8.row > div:nth-child(1) > img').style.transform = 'scale(1)';
+    document.querySelector('#team > div > div.col-md-8.row > div:nth-child(2) > img').style.transform = 'scale(1)';
+    document.querySelector('#team > div > div.col-md-8.row > div:nth-child(3) > img').style.transform = 'scale(1)';
+}
+
+/*----------------------------------------------------------------------*/
+
+function projectMyonmou1() {
+    document.querySelector('#portfolio > div.row.ourPortfolio-listItem > div:nth-child(1) > img').style.transform = 'scale(1.2)';
+}
+function projectMyonmou2() {
+    document.querySelector('#portfolio > div.row.ourPortfolio-listItem > div:nth-child(2) > img').style.transform = 'scale(1.2)';
+}
+function projectMyonmou3() {
+    document.querySelector('#portfolio > div.row.ourPortfolio-listItem > div:nth-child(3) > img').style.transform = 'scale(1.2)';
+}
+function projectMyonmou4() {
+    document.querySelector('#portfolio > div.row.ourPortfolio-listItem > div:nth-child(4) > img').style.transform = 'scale(1.2)';
+}
+function projectMyonmou5() {
+    document.querySelector('#portfolio > div.row.ourPortfolio-listItem > div:nth-child(5) > img').style.transform = 'scale(1.2)';
+}
+function projectMyonmou6() {
+    document.querySelector('#portfolio > div.row.ourPortfolio-listItem > div:nth-child(6) > img').style.transform = 'scale(1.2)';
+}
+function projectMyonmou7() {
+    document.querySelector('#portfolio > div.row.ourPortfolio-listItem > div:nth-child(7) > img').style.transform = 'scale(1.2)';
+}
+function projectMyonmou8() {
+    document.querySelector('#portfolio > div.row.ourPortfolio-listItem > div:nth-child(8) > img').style.transform = 'scale(1.2)';
+}
+
+function projectMyout() {
+    document.querySelector("#portfolio > div.row.ourPortfolio-listItem > div:nth-child(1) > img").style.transform = 'scale(1)';
+    document.querySelector("#portfolio > div.row.ourPortfolio-listItem > div:nth-child(2) > img").style.transform = 'scale(1)';
+    document.querySelector("#portfolio > div.row.ourPortfolio-listItem > div:nth-child(3) > img").style.transform = 'scale(1)';
+    document.querySelector("#portfolio > div.row.ourPortfolio-listItem > div:nth-child(4) > img").style.transform = 'scale(1)';
+    document.querySelector("#portfolio > div.row.ourPortfolio-listItem > div:nth-child(5) > img").style.transform = 'scale(1)';
+    document.querySelector("#portfolio > div.row.ourPortfolio-listItem > div:nth-child(6) > img").style.transform = 'scale(1)';
+    document.querySelector("#portfolio > div.row.ourPortfolio-listItem > div:nth-child(7) > img").style.transform = 'scale(1)';
+    document.querySelector("#portfolio > div.row.ourPortfolio-listItem > div:nth-child(8) > img").style.transform = 'scale(1)';
+}
+
+      
